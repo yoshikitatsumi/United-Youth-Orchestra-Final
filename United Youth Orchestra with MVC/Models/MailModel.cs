98d552b9e6a89c622c87393d09d4ac638e6ee137
Yoshi Tatsumi <YT007@atcltd.atc.org.nz>
2021-02-12 13:22:35 +1300

    initial commit

---
+++ United Youth Orchestra with MVC/Models/MailModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace United_Youth_Orchestra_with_MVC.Models
+{
+    public class MailModel
+    {
+        public string From
+        {
+            get;
+            set;
+        }
+        public string To
+        {
+            get;
+            set;
+        }
+        public string Subject
+        {
+            get;
+            set;
+        }
+        public string Body
+        {
+            get;
+            set;
+        }
+    }
+}
